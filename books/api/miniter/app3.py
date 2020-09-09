84665cfcf61766cdb1cc8133728ee4c23303aca1
minchae3618@gmail.com <minchae3618@gmail.com>
2020-09-09 22:28:19 +0900

    add certi

--- books/api/miniter/app3.py
+++ books/api/miniter/app3.py
@@ -1,13 +1,19 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text 
-
-
+import bcrypt
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
 
 
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
     new_user = request.json
+    new_user['password'] = bcrypt.hashpw(
+        new_user['password'].encode('UTF-8'),
+        bcrypt.gensalt()
+    )
     new_user_id = app.database.execute(text("""
         insert into users (
             name,
@@ -21,53 +27,60 @@ def sign_up():
             :password
         )
     """), new_user).lastrowid
+    new_user_info = get_user(new_user_id)
 
-    row = current_app.database.execute(text("""
-            select 
+    return jsonify(new_user_info)
+
+@app.route('./login', methods=['POST'])
+def login():
+    credential = request.json
+    email = credential['email']
+    password = credential['password']
+
+    row = database.execute(text("""
+        select
             id,
-            name,
-            email,
-            profile
-            from users
-            where id = :user_id
-    """), {
-        'user_id' : new_user_id
-    }).fetchone()
+            hashed_password
+        from users
+        where email = :email
+    """), {'email' : email}).fetchall()
 
-    create_user = {
-        'id' : row['id'],
-        'name' : row['name'],
-        'email' : row['email'],
-        'profile' : row['profile']
-    } if row else None 
-    return jsonify(create_user)
+    if row and bcrypt.checkpw(password.encode('UTF-8'), row['hashed_password'].encode('UTF-8')):
+        user_id = row['id']
+        payload = {
+            'user_id' : user_id
+            'exp' : datetime.utcnow() + timedelta(seconds = 60 * 60 * 24)
+        }
+        token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256')
+        return jsonify({
+            'access_token' : token.decode('UTF-8')
+        })
+    else:
+        return ', 401'
+
+
 
 @app.route('/tweet', methods=['POST'])
+@login_required
 def tweet():
     user_tweet = request.json
+    user_tweet['id'] = g.user_id
     tweet = user_tweet['tweet']
 
     if len(tweet) > 300:
         return '300자를 초과했습니다.' 400
-    app.database.execute(text("""
-        insert into tweets (
-            uesr_id,
-            tweet
-        ) values(
-            :id,
-            :tweet
-        )
-    """), user_tweet)
-
+    insert_tweet(user_tweet)
     return "", 200
 
 @app.route('/follow', methods=['POST'])
+@login_required
 def follow():
     payload = request.json 
     insert_follow(payload)
     return '', 200
 
 @app.route('/unfollow', methods=['POST'])
+@login_required
 def unfollow():
     payload = request.json 
     insert_unfollow(payload)
@@ -197,3 +210,38 @@ class CustomJSONEncoder(JSONEncoder):
         if isinstance(obj, set):
             return list(obj)
         return JSONEncoder.default(self, obj)
+
+
+### test
+# def test_decorator(f):
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         print('Decorated Function')
+#         return f(*args, **kwargs)
+#     return decorated_function
+
+# @test_decorator
+# def func():
+#     print('Calling funct function')
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*arg, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
+            except jwt.InvalidTokenError:
+                payload = None 
+            
+            if payload is None : return Response(status=401):
+            
+            user_id = payload['user_id']
+            g.user_id = user_id
+            g.user = get_user_info(user_id) if user_id else None 
+        else:
+            return Response(status= 401)
+        
+        return f(*args, **kwargs)
+    return decorated_function
+
