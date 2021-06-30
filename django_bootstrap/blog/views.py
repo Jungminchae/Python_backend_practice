468a5c9a3124b0f3c41f5cf8dcf1dceaddf4f875
Jungminchae <minchae3618@gmail.com>
2021-06-30 13:10:18 +0900

    add: loginmixin

--- django_bootstrap/blog/views.py
+++ django_bootstrap/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Category, Tag
 
 # listview 쓸거임
@@ -76,7 +77,7 @@ def tag_page(request, slug):
     )
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = [
         "title",
@@ -86,3 +87,11 @@ class PostCreateView(CreateView):
         "file_upload",
         "category",
     ]
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreateView, self).form_valid(form)
+        else:
+            return redirect("/blog/")
