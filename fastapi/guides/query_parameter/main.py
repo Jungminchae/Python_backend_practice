950338ab9b4a6346cf8acf84ed392b68aa94392a
Jungminchae <minchae3618@gmail.com>
2021-08-13 23:05:10 +0900

    add: query parameter

---
+++ fastapi/guides/query_parameter/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+# 쿼리 parameter
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# 선택적 parameter
+@app.get("/items/{item_id}")
+async def read_items(item_id: str, q: Optional[str] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
