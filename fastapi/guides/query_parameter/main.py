00cb551a8dd08095e12d780964fe4a0facf7a814
Jungminchae <minchae3618@gmail.com>
2021-08-21 23:18:09 +0900

    add: query parameter

--- fastapi/guides/query_parameter/main.py
+++ fastapi/guides/query_parameter/main.py
@@ -13,8 +13,49 @@ async def read_item(skip: int = 0, limit: int = 10):
 
 
 # 선택적 parameter
+# @app.get("/items/{item_id}")
+# async def read_items(item_id: str, q: Optional[str] = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+# bool 형 선언
 @app.get("/items/{item_id}")
-async def read_items(item_id: str, q: Optional[str] = None):
+async def read_items(item_id: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id}
     if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+# 다중 경로/쿼리 매개변수 -> 선택적
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({"description": "This is an amazing that has a long description"})
+    return item
+
+
+# 쿼리 매개변수 필수로
+@app.get("/item/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+
+# 일부는 선택, 일부는 필수
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: Optional[int] = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
