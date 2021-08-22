d8d2fe22a64fd1b090be1d302884ed150410bd24
Jungminchae <minchae3618@gmail.com>
2021-08-22 21:33:18 +0900

    add: pram task

---
+++ fastapi/guides/query_parameters&string_validation/main.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+# @app.get("/items/")
+# async def read_items(q: Optional[str]=None):
+#     results = {"items" : [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# additional validation
+@app.get("/items/")
+async def read_items(q: Optional[str] = Query(None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
