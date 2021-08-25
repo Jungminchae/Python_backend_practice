4521ffa2cee3f33a8c724affc8cfd257f6977dc2
Jungminchae <minchae3618@gmail.com>
2021-08-25 23:54:28 +0900

    add query param & string validation

--- fastapi/guides/query_parameters&string_validation/main.py
+++ fastapi/guides/query_parameters&string_validation/main.py
@@ -12,8 +12,16 @@ app = FastAPI()
 
 # additional validation
 @app.get("/items/")
-async def read_items(q: Optional[str] = Query(None, max_length=50)):
+async def read_items(
+    q: Optional[str] = Query(None, max_length=50)
+):  # min_length=3, regex="^fixedquery$" 등 다른 매개변수도 넣을 수 있음
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
     return results
+
+
+# Query(..., min_length=3) 처럼 사용하여 required하게 만들 수 있음, ...은 파이썬 Ellipsis
+# q: Optional(List[str] = Query(None)) 으로 주고 q를 리스트 형태로 여러개 원소를 가진 녀석으로 받을 수 있음, /items/?q=blah&q=blah2
+# 그냥 Query([]) 를 바로 넣을 수 있고 그 안에 값을 기본값을 줄 수도 있음
+# q : Optional[str] = Query(ailias =blahblah) alias를 줄 수도 있음
